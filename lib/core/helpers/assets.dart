76009fb256bbb7f83aff699f6d15b6a2bf7027e2
Moustafa Hussein <mustafahussein1228@gmail.com>
2025-06-20 14:28:11 +0300

    Installing go router package for application routing , installing device preview package for responsive design across all devices

---
+++ lib/core/helpers/assets.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: prefer_single_quotes
+class Images {
+  Images._();
+
+  /// Assets for ImagesImagesSplashScreen
+  /// assets/images/splash_screen.png
+  static const String imagesImagesSplashScreen =
+      "assets/images/splash_screen.png";
+}
---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,33 @@
+import 'package:docdoc_app/features/splash/presentation/views/splash_view.dart';
+import 'package:go_router/go_router.dart';
+
+abstract class AppRouter {
+  static const kOnBoardView = '/onBoardView';
+  static const kSplashView = '/';
+  static const kLoginView = '/loginView';
+
+  static GoRouter router = GoRouter(
+    routes: [
+      GoRoute(
+        path: kSplashView,
+        builder: (context, state) => const SplashView(),
+      ),
+      // GoRoute(
+      //   path: kOnBoardView,
+      //   pageBuilder: (context, state) {
+      //     return CustomTransitionPage(
+      //       child: const OnBoardView(),
+      //       transitionsBuilder: (
+      //         context,
+      //         animation,
+      //         secondaryAnimation,
+      //         child,
+      //       ) {
+      //         return FadeTransition(opacity: animation, child: child);
+      //       },
+      //     );
+      //   },
+      // ),
+    ],
+  );
+}
