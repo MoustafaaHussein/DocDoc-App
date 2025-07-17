d6f6870ffe24f42e3e80163b8b55bb5850892c21
Moustafa Hussein <mustafahussein1228@gmail.com>
2025-07-17 17:57:50 +0300

    Update pubspec.lock for package version upgrades; add empty files for personalized recommendation views

---
+++ lib/features/recomendation/presentation/views/presonalize_recomendation_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class PresonalizeRecomendationView extends StatelessWidget {
+  const PresonalizeRecomendationView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/features/recomendation/presentation/views/widgets/presonalize_recomendation_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class PresonalizeRecomendationViewBody extends StatelessWidget {
+  const PresonalizeRecomendationViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
